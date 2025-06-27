9b19a16b02fae91d49472ad43bc4a64f5d877e94
EllieZhou1 <ellie.m.zhou@gmail.com>
2025-06-27 19:22:13 -0400

    - Added train/test loss/accuracy to wandb to log - Printed train dataset length and validation dataset length - Added validation to the training loop

--- slowfast.py
+++ slowfast.py
@@ -38,21 +38,36 @@ with open(f"config/{args.config:04}.yaml", "r") as f:
 CONFIG['device'] = "cuda" if torch.cuda.is_available() else "cpu"
 
 # 1. load the model 
-model = torch.hub.load('facebookresearch/pytorchvideo', 'slowfast_r50', pretrained=False)
+my_model = torch.hub.load('facebookresearch/pytorchvideo', 'slowfast_r50', pretrained=False)
 # 2. Move to device
-model = model.to(CONFIG['device'])
+my_model = my_model.to(CONFIG['device'])
 # 3. Wrap with DataParallel if multiple GPUs
 if torch.cuda.device_count() > 1:
     print(f"Using {torch.cuda.device_count()} GPUs")
-    model = nn.DataParallel(model)
+    my_model = nn.DataParallel(my_model)
 
 #Initiate the Wandb
 run = wandb.init(
     project="Slowfast_Kinetics",
-    config=CONFIG
+    config=CONFIG,
 )
 
-wandb.watch(model, log='all', log_freq = 100)
+run.define_metric("train loss", step_metric="train_step")
+run.define_metric("train accuracy", step_metric="train_step")
+run.define_metric("test loss", step_metric="test_step")
+run.define_metric("test accuracy", step_metric="test_step")
+
+run.define_metric("train loss (epoch avg)", step_metric="epoch")
+run.define_metric("train accuracy (epoch avg)", step_metric="epoch")
+run.define_metric("test loss (epoch avg)", step_metric="epoch")
+run.define_metric("test accuracy (epoch avg)", step_metric="epoch")
+
+
+train_step = 0
+test_step = 0
+
+
+wandb.watch(my_model, log='all', log_freq = 100)
 
 print(torch.version.cuda)  # Should print a CUDA version, not None
 print(torch.cuda.is_available())
@@ -68,36 +83,38 @@ train_dataset = KineticsDataset2(
 )
 
 #create a dataset instance for validation set
-# validation_dataset = KineticsDataset2(
-#     csv_path = os.path.join(CONFIG['metadata_dir'], "clean_validate.csv"),
-#     split="validate",
-#     max_videos=None
-# )
+validation_dataset = KineticsDataset2(
+     csv_path = os.path.join(CONFIG['metadata_dir'], "clean_validate.csv"),
+     split="validate",
+     max_videos=None
+)
 
-print("Made dataset. Length of training dataset is ", len(train_dataset))
+train_len = len(train_dataset)
+validation_len = len(validation_dataset)
+
+print("Made dataset. Length of training dataset is ", train_len)
+print("Made dataset. Length of validation dataset is ", validation_len)
+
 
 #my_train_dataloader = torch.utils.data.DataLoader(train_dataset, CONFIG['batch_size'], shuffle=True)
 my_train_dataloader = torch.utils.data.DataLoader(train_dataset, CONFIG['batch_size'], 
                                                   num_workers=8, pin_memory=True, shuffle=True)
-# my_validation_dataloader = torch.utils.data.DataLoader(validation_dataset, CONFIG['batch_size'], shuffle=False)
 
-print("Made dataloader")
+my_validation_dataloader = torch.utils.data.DataLoader(validation_dataset, CONFIG['batch_size'], 
+                                                       num_workers=8, pin_memory=True, shuffle=False)
 
+print("Made dataloaders")
 
-# ========== TRAINING PIPELINE ==========
-def train_model():
-    # Initialize components
-    optimizer = optim.SGD(model.parameters(), lr=CONFIG['learning_rate'])
-    loss_fn = nn.CrossEntropyLoss(reduction='mean')
-    
-    # Training loop
-    for epoch in range(CONFIG['num_epochs']):
-        print("Starting epoch ", epoch)
-        model.train()
-        train_loss = 0.0
+my_optimizer = optim.SGD(my_model.parameters(), lr=CONFIG['learning_rate'])
+my_loss_fn = nn.CrossEntropyLoss(reduction='mean')
 
-        # Training phase
-        for i, batch in enumerate(my_train_dataloader):
+def train_epoch(model, epoch, optimizer, loss_fn, dataloader):
+    global train_step
+    train_loss = 0.0
+    correct = 0
+    total = 0
+
+    for i, batch in enumerate(dataloader):
             batch_size = batch["inputs"][0].shape[0]  # Number of samples in the batch                                         
             inputs = [x.to(CONFIG['device']) for x in batch["inputs"]]
             labels = batch["label"].to(CONFIG['device'])
@@ -109,24 +126,94 @@ def train_model():
             loss = loss_fn(outputs, labels) #the avg of the losses for the samples in the batch
             loss.backward()
             optimizer.step()
-        
+            
             train_loss += loss.item()
 
             predicted = torch.argmax(outputs, dim=1)
-            correct = (predicted == labels).sum().item()
-            accuracy = float(correct)/batch_size
+            correct += (predicted == labels).sum().item() #add the number of correct predictions in this batch
+            total += batch_size #add the number of samples in this batch
+            accuracy = float(correct)/total
 
-
-            #Log performance every 20 batches
-            if True:
+            #Log performance every 50 batch
+            if i % 20 == 0:
                 now = datetime.now()
-                run.log({"epoch": epoch, "loss": loss, "accuracy": accuracy, "correct": correct})
-                print("Epoch {}, Batch {}, Loss {:.4f}, Correct {}, Total {}, Time {}".
-                      format(epoch, i, loss, correct, batch_size, now.strftime("%Y-%m-%d %H:%M:%S")))
-  
+                run.log({
+                     "train loss": loss, 
+                     "train accuracy": accuracy,
+                     "train_step": train_step
+                })
+                train_step += 1
 
-        file_path = f"/n/fs/visualai-scr/temp_LLP/ellie/slowfast_kinetics/weights_{epoch:06d}.pth"
-        torch.save(model.state_dict(), file_path)
+                print("Epoch {}, Batch {}, Train Loss {:.4f}, Train Accuracy {:.3f}, Time {}".
+                    format(epoch, i, train_loss/(i+1), accuracy, now.strftime("%Y-%m-%d %H:%M:%S")))
+                
+    run.log({
+        "train loss (epoch avg)": train_loss/len(dataloader),
+        "train accuracy (epoch avg)": float(correct)/total,
+        "epoch": epoch
+    })
+                
+
+def test_epoch(model, epoch, loss_fn, dataloader):
+    global test_step
+    test_loss = 0.0
+    correct = 0
+    total = 0
+
+    for i, batch in enumerate(dataloader):
+            batch_size = batch["inputs"][0].shape[0]  # Number of samples in the batch                                         
+            inputs = [x.to(CONFIG['device']) for x in batch["inputs"]]
+            labels = batch["label"].to(CONFIG['device'])
+
+            #print("         Starting batch ", i, " with batch size ", batch_size)
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels) #the avg of the losses for the samples in the batch
+            
+            test_loss += loss.item()
+
+            predicted = torch.argmax(outputs, dim=1)
+            correct += (predicted == labels).sum().item() #add the number of correct predictions in this batch
+            total += batch_size #add the number of samples in this batch
+            accuracy = float(correct)/total
+
+            #Log performance every 50 batch
+            if i % 20 == 0:
+                now = datetime.now()
+                run.log({
+                     "test loss": loss, 
+                     "test accuracy": accuracy,
+                     "test_step": test_step
+                })
+                test_step += 1
+
+                print("Epoch {}, Batch {}, Test Loss {:.4f}, Test Accuracy {:.3f}, Time {}".
+                    format(epoch, i, test_loss/(i+1), accuracy, now.strftime("%Y-%m-%d %H:%M:%S")))
+
+    run.log({
+        "test loss (epoch avg)": test_loss/len(dataloader),
+        "test accuracy (epoch avg)": float(correct)/total,
+        "epoch": epoch
+    })
+
+# ========== TRAINING PIPELINE ==========
+def train_model():
+    batches_needed = train_len // CONFIG['batch_size']
+    print(f"Training with {batches_needed} batches per epoch")
+
+    for epoch in range(CONFIG['num_epochs']):
+        print("Starting epoch ", epoch)
+
+        my_model.train()
+        train_epoch(my_model, epoch, my_optimizer, my_loss_fn, my_train_dataloader)
+
+        print("Finish epoch ", epoch, "training. Starting validation")
+
+        my_model.eval()
+        test_epoch(my_model, epoch, my_loss_fn, my_validation_dataloader)
+        
+        file_path = f"/n/fs/visualai-scr/temp_LLP/ellie/slowfast_kinetics/saved_weights/slowfast_kinetics400/weights_{epoch:06d}.pth"
+        torch.save(my_model.state_dict(), file_path)
+            
 
 if __name__ == "__main__":
     train_model()
