2ddd3a71767ea3fccce44b7173fa5ca1ea45869c
EllieZhou1 <ellie.m.zhou@gmail.com>
2025-06-27 19:19:30 -0400

    - Made is so that when you call a job, you can pass 0, 1, 2, 3, or 4 to   specify which yaml file

--- myjob.sh
+++ myjob.sh
@@ -2,15 +2,15 @@
 ##SBATCH --account=visualai    # Specify VisualAI
 #SBATCH --nodes=1             # nodes requested
 #SBATCH --ntasks=1            # tasks requested
-#SBATCH --cpus-per-task=20     # Specify the number of CPUs your task will need.
+#SBATCH --cpus-per-task=80     # Specify the number of CPUs your task will need.
 #SBATCH --gres=gpu:8          # the number of GPUs requested
 #SBATCH --mem=400G             # memory 
 #SBATCH -o /n/fs/visualai-scr/temp_LLP/ellie/slowfast_kinetics/outfile        # send stdout to outfile
 #SBATCH -e /n/fs/visualai-scr/temp_LLP/ellie/slowfast_kinetics/errfile  # send stderr to errfile
-#SBATCH -t 60:00:00           # time requested in hour:minute:second
+#SBATCH -t 168:00:00           # time requested in hour:minute:second
 #SBATCH --mail-type=all       # choice between begin, end, all to notify you via email
 #SBATCH --mail-user=ez9517@princeton.edu
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ellie_env
-python slowfast.py --config 0
+python slowfast.py --config $1
